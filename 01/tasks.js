8908d03ecc5db54683c718a23c458f9e87c782db
Наталия Сергеевна Бобровская <nataliya.bobrovskaya@phystech.edu>
2017-09-27 22:36:05 +0300

    hw1 travis2

--- 01/tasks.js
+++ 01/tasks.js
@@ -5,20 +5,22 @@
  * '1 и 6.45, -2, но 8, а затем 15, то есть 2.7 и -1028' => { min: -1028, max: 15 }
  */
 function getMinMax(str) {
-  let arr = str.split(/[ ,!?:;]/)
+  let arr = str.split(/[ ,!?:;]/);
+
   for (let i = 0; i < arr.length; i++) {
     if (isNaN(+arr[i]) || (arr[i].length === 0)) {
-      arr.splice(i, 1)
-      --i
+      arr.splice(i, 1);
+      --i;
     }
   }
-  let min = arr[0]
-  let max = arr[0]
+  let min = arr[0];
+  let max = arr[0];
+
   for (let i = 1; i < arr.length; i++) {
-    min = Math.min(min,arr[i])
-    max = Math.max(max,arr[i])
+    min = Math.min(min,arr[i]);
+    max = Math.max(max,arr[i]);
   }
-  return {min: min, max:max}
+  return {min: min, max:max};
 }
 
 /* ============================================= */
@@ -30,9 +32,9 @@ function getMinMax(str) {
  */
 function fibonacciSimple(n) {
   if (n===0)
-    return 0
+    return 0;
   if (n===1)
-    return 1
+    return 1;
   return fibonacciSimple(n-1)+fibonacciSimple(n-2);
 }
 /* ============================================= */
@@ -44,23 +46,23 @@ function fibonacciSimple(n) {
  * @return {number} число под номером х
  */
 function fibonacciWithCache(n) {
-  let cache = {}
+  let cache = {};
 
   function fibonacci(n) {
-    let value
+    let value;
       if (n in cache)
-        value = cache[n]
+        value = cache[n];
       else {
         if (n === 0 || n === 1)
-          value = n
+          value = n;
         else
           value = fibonacci(n - 1) + fibonacci(n - 2)
-        cache[n] = value
+        cache[n] = value;
       }
-    return value
+    return value;
   }
 
-  return fibonacci(n)
+  return fibonacci(n);
 }
 
 /* ============================================= */
@@ -81,29 +83,30 @@ function fibonacciWithCache(n) {
  * @return {string}
  */
 function printNumbers(max, cols) {
-  let num_str = Math.ceil((max+1)/cols)
-  let final_string = ''
-  flag = false
+  let num_str = Math.ceil((max+1)/cols);
+  let final_string = '';
+  let flag = false;
+
   for (let i = 0; i < num_str; i++){
     for (let j = 0; j < cols; j++){
       if (i + j*num_str <= max)
         if (i + j*num_str < 10)
-          final_string += ' ' + (i + j*num_str)
+          final_string += ' ' + (i + j*num_str);
         else
-          final_string += i + j*num_str
+          final_string += i + j*num_str;
       if (i + j*num_str === max) {
-        flag = true
+        flag = true;
         break;
       }
       if (j < cols-1 && !flag)
-        final_string += ' '
+        final_string += ' ';
     }
     if (flag)
       break;
     if (i < num_str - 1 && !flag)
-      final_string += '\n'
+      final_string += '\n';
   }
-  return final_string
+  return final_string;
 }
 
 /* ============================================= */
@@ -115,25 +118,27 @@ function printNumbers(max, cols) {
  */
 function rle(input) {
   let final_str = ''
+
   if (input.length > 0) {
-    let cur_sym = input[0]
-    let count = 1
+    let cur_sym = input[0];
+    let count = 1;
+
     for (let i = 1; i < input.length; i++) {
       if (input[i] === cur_sym)
-        ++count
+        ++count;
       else {
-        final_str += cur_sym
+        final_str += cur_sym;
         if (count > 1)
-          final_str += count
-        count = 1
-        cur_sym = input[i]
+          final_str += count;
+        count = 1;
+        cur_sym = input[i];
       }
     }
-    final_str += cur_sym
+    final_str += cur_sym;
     if (count > 1)
-      final_str += count
+      final_str += count;
   }
-  return final_str
+  return final_str;
 }
 
 
