c13cd67684b727e3395889871afda98ede0e9943
bobrovskayaa <nataliya.bobrovskaya@phystech.edu>
2017-10-12 22:14:03 +0300

    checkstyle

--- 03/5/task.js
+++ 03/5/task.js
@@ -9,14 +9,13 @@
 const dict = {
   I: 1, IV: 4, V: 5, IX: 9, X: 10, XL: 40, L: 50, XC: 90, C: 100, CD: 400, D: 500, CM: 900, M: 1000
 };
+
 function strIntoNum(str) {
   let finalNum = 0;
   const checkedSymb = [];
-  const finalArray = [];
 
   if (str.length === 1) {
-    finalNum += dict[str[0]];
-    checkedSymb[0] = 1;
+    return dict[str[0]];
   }
   for (let i = 1; i < str.length; ++i) {
     if (str[i - 1] + str[i] in dict && checkedSymb[i - 1] !== 1 && checkedSymb[i] !== 1) {
@@ -35,7 +34,9 @@ function strIntoNum(str) {
 }
 
 function strIntoArray(str) {
+  const finalArray = [];
   const finalNum = strIntoNum(str);
+
   for (let i = 0; i < finalNum; ++i) {
     finalArray.push(i);
   }
