35f1f7a53daa1b8d71f307be551266eb7b34e930
bobrovskayaa <nataliya.bobrovskaya@phystech.edu>
2017-10-19 23:32:32 +0300

    hw4 task1,4

---
+++ 04/04/scripts.js
@@ -0,0 +1,8 @@
+var share = Ya.share2('my-share', {
+  content: {
+    url: 'https://bobrovskayaa.github.io/Fintech-Javascript-3/04/01/',
+    title: 'Блог Наташи про Паланика',
+    description: 'Да-да-да. Это правда. Наташа пишет про Паланика.',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg'               
+  }
+});
