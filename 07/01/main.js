bb9efab15ab249920f4363c4ccff3f4723dc4988
bobrovskayaa <nataliya.bobrovskaya@phystech.edu>
2017-11-13 02:12:28 +0300

    hw7 checkstyle-task1

--- 07/01/main.js
+++ 07/01/main.js
@@ -5,43 +5,21 @@ function inputMask(str) {
   let currentStr = '+ 7 (';
 
   function inputMaskСonditions(initLen) {
-    if (initLen === 1) {
-      currentStr = '+ 7 (' + str[0] + ' ';
-    }
-    if (initLen >= 6) {
-      currentStr += str[5] + ' ';
-    }
-    if (initLen >= 8) {
-      currentStr += str[7] + ' ';
-    }
-    if (initLen >= 10) {
-      currentStr += str[9] + ') - ';
-    }
-    if (initLen >= 15) {
-      currentStr += str[14] + ' ';
-    }
-    if (initLen >= 17) {
-      currentStr += str[16] + ' ';
-    }
-    if (initLen >= 19) {
-      currentStr += str[18] + ' - ';
-    }
-    if (initLen >= 23) {
-      currentStr += str[22] + ' ';
-    }
-    if (initLen >= 25) {
-      currentStr += str[24] + ' - ';
-    }
-    if (initLen >= 29) {
-      currentStr += str[28] + ' ';
-    }
-    if (initLen >= 31) {
-      currentStr += str[30];
-    }
+    if (initLen === 1) { currentStr = '+ 7 (' + str[0] + ' '; }
+    if (initLen >= 6) { currentStr += str[5] + ' '; }
+    if (initLen >= 8) { currentStr += str[7] + ' '; }
+    if (initLen >= 10) { currentStr += str[9] + ') - '; }
+    if (initLen >= 15) { currentStr += str[14] + ' '; }
+    if (initLen >= 17) { currentStr += str[16] + ' '; }
+    if (initLen >= 19) { currentStr += str[18] + ' - '; }
+    if (initLen >= 23) { currentStr += str[22] + ' '; }
+    if (initLen >= 25) { currentStr += str[24] + ' - '; }
+    if (initLen >= 29) { currentStr += str[28] + ' '; }
+    if (initLen >= 31) { currentStr += str[30]; }
   }
 
   if (initLen === 0 || initLen > 1 && initLen < 6) {
-      currentStr = '+ 7 (';
+    currentStr = '+ 7 (';
   }
   else {
     inputMaskСonditions(initLen);
