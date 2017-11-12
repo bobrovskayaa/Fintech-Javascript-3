9dab495a82165ee9d272c031c9b46ee4071b57db
bobrovskayaa <nataliya.bobrovskaya@phystech.edu>
2017-11-13 01:52:53 +0300

    hw7 checkstyle-task1

--- 07/01/main.js
+++ 07/01/main.js
@@ -1,56 +1,44 @@
 let input = document.getElementById('phone');
 
 function inputMask(str) {
+  const initLen = str.length;
+  let currentStr = '+ 7 (';
   console.log(str.length);
-  console.log(str);
-  switch (true) {
-    case (str.length === 0 || str.length > 1 && str.length < 6):
-      return '+ 7 (';
-    case (str.length === 1):
-      return '+ 7 (' + str[0] + ' ';
-    case (str.length >= 6 && str.length < 8):
-      return '+ 7 (' + str[5] + ' ';
-    case (str.length >= 8 && str.length < 10):
-      return '+ 7 (' + str[5] + ' ' + str[7] + ' ';
-    case (str.length >= 10 && str.length < 15):
-      return '+ 7 (' + str[5] + ' ' + str[7] + ' ' + str[9] + ') - ';
-    case (str.length >= 15 && str.length < 17):
-      return '+ 7 (' + str[5] + ' ' + str[7] + ' ' + str[9] + ') - ' +
-        str[14] + ' ';
-    case (str.length >= 17 && str.length < 19):
-      return '+ 7 (' + str[5] + ' ' + str[7] + ' ' + str[9] + ') - ' +
-        str[14] + ' ' + str[16] + ' ';
-    case (str.length >= 19 && str.length < 23):
-      return '+ 7 (' + str[5] + ' ' + str[7] + ' ' + str[9] + ') - ' +
-        str[14] + ' ' + str[16] + ' ' + str[18] + ' - ';
-    case (str.length >= 23 && str.length < 25):
-      return '+ 7 (' + str[5] + ' ' + str[7] + ' ' + str[9] + ') - ' +
-        str[14] + ' ' + str[16] + ' ' + str[18] + ' - ' + str[22] +
-        ' ' ;
-    case (str.length >= 25 && str.length < 29):
-      return '+ 7 (' + str[5] + ' ' + str[7] + ' ' + str[9] + ') - ' +
-        str[14] + ' ' + str[16] + ' ' + str[18] + ' - ' + str[22] + 
-        ' ' + str[24] + ' - ';
-    case (str.length >= 29 && str.length < 31):
-      return '+ 7 (' + str[5] + ' ' + str[7] + ' ' + str[9] + ') - ' +
-        str[14] + ' ' + str[16] + ' ' + str[18] + ' - ' + str[22] + 
-        ' ' + str[24] + ' - ' + str[28] + ' ';
-    case (str.length >= 31):
-      return '+ 7 (' + str[5] + ' ' + str[7] + ' ' + str[9] + ') - ' +
-        str[14] + ' ' + str[16] + ' ' + str[18] + ' - ' + str[22] + 
-        ' ' + str[24] + ' - ' + str[28] + ' ' + str[30];
-    default:
-      alert( 'oops' );
-      return '';
+
+  if (initLen === 0 || initLen > 1 && initLen < 6)
+      currentStr = '+ 7 (';
+  else {
+    if (initLen === 1)
+      currentStr = '+ 7 (' + str[0] + ' ';
+    if (initLen >= 6)
+      currentStr += str[5] + ' ';
+    if (initLen >= 8)
+      currentStr += str[7] + ' ';
+    if (initLen >= 10)
+      currentStr += str[9] + ') - ';
+    if (initLen >= 15)
+      currentStr += str[14] + ' ';
+    if (initLen >= 17)
+      currentStr += str[16] + ' ';
+    if (initLen >= 19)
+      currentStr += str[18] + ' - ';
+    if (initLen >= 23)
+      currentStr += str[22] + ' ' ;
+    if (initLen >= 25)
+      currentStr += str[24] + ' - ';
+    if (initLen >= 29)
+      currentStr += str[28] + ' ';
+    if (initLen >= 31)
+      currentStr += str[30];
   }
+  return currentStr;
 }
 
 function inputUpdateHandler() {
   let input = document.getElementById('phone');
   let output = document.getElementById('result');
-  console.log(input.value);
-  console.log(/[^0-9+-\s()]/.test(input.value));
   let newLine = '';
+
   if (!/[^0-9+-\s()]/.test(input.value)) {
     newLine = inputMask(input.value);
   }
